6293c99e417ce411b3c6519722a402ef03228b24
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-21 20:04:30 +0200

    first task done

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesAmount = categories.children;
+console.log(`Number of categories: ${categoriesAmount.length}`);
+
+const categoriesArray = document.querySelectorAll(".item");
+
+categoriesArray.forEach((category, index) => {
+  let categoryHeader = categoriesArray[index].firstElementChild.textContent;
+  let categoryElementsAmount =
+    categoriesArray[index].querySelector("ul").children.length;
+  console.log(`Category: ${categoryHeader}`);
+  console.log(`Number of elements: ${categoryElementsAmount}`);
+});
