b0c4637550abf42ca844d5b7351721cbd5e00768
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-24 21:21:05 +0200

    task 10 in progress

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const divBox = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", (event) => {
+  createBoxes(document.querySelector("input[type='number']").value);
+});
+
+destroyBtn.addEventListener("click", (event) => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const divString = `<div style='background-color="${getRandomHexColor()}" display="block" width="${boxSize}px" height="${boxSize}px"'></div>`;
+    boxSize += 10;
+    divBox.insertAdjacentHTML("beforeend", `${divString}`);
+  }
+}
+
+function destroyBoxes() {
+  divBox.textContent = "";
+  // while (divBox.firstChild) {
+  //   myNode.removeChild(divBox.lastChild);
+  // }
+}
