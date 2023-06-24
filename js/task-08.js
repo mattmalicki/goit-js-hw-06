5a7abda923cecb31739dd76b9501b38a09e7c671
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-24 09:40:23 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const login = document.querySelector(".login-form");
 
+function checkLogin(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value} password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+login.addEventListener("submit", checkLogin);
