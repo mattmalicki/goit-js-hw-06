efdbd61f048efdfa4d364d4efabda692d16d9673
mattmalicki <mateusz.malicki96@gmail.com>
2023-06-25 10:55:35 +0200

    making task 7 better

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,11 @@
 const text = document.querySelector("#text");
 const range = document.querySelector("#font-size-control");
+// Those commented lines will match range value to the font size:
+// ---when page is loading the range is set in the middle ao this will match font size to the begining value
+// text.style.fontSize = `${range.value}px`;
+// ---this will set begining value to match font size witch is 16
+// range.setAttribute("value", "16");
+
 range.addEventListener("change", sizeChange);
 
 function sizeChange() {
